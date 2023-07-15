a5ea37bb5e4b586acaeff61d0f643e23119f2daa
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-07-15 13:54:09 -0700

    added javascript code

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,15 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
     // Pizza constructor
     function Pizza(toppings, size) {
-      this.toppings = toppings;
-      this.size = size;
-      console.log('Pizza object created:', this);
+        this.toppings = toppings;
+        this.size = size;
+        console.log('Pizza object created:', this);
     }
-  
+    // Prototype method to calculate the cost of a pizza
+    Pizza.prototype.calculateCost = function () {
+        // Define the base cost for each size
+        const sizeCosts = {
+            small: 8,
+            medium: 10,
+            large: 12
+        };
